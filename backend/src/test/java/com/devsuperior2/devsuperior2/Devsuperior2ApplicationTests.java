07136a8cf5adf8131b6eb74afeab555be9e2f02c
Gamariksy <ramosdesouzasamuel7@gmail.com>
2022-03-11 18:11:50 -0400

    Project CREATED

---
+++ backend/src/test/java/com/devsuperior2/devsuperior2/Devsuperior2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior2.devsuperior2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devsuperior2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
